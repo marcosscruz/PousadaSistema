c5b118ae50fb3becdd5ad9da40f1b995d313e998
Marcela <marcos.v.s.cruz10@gmail.com>
2023-07-16 21:01:14 -0300

    Update Despesa.java
    criada para fazer o lançamento da receita do mês e ver o balanço mensal

--- src/main/java/br/com/pousada/servicos/Despesa.java
+++ src/main/java/br/com/pousada/servicos/Despesa.java
@@ -1,5 +1,57 @@
 package br.com.pousada.servicos;
 
+import java.time.LocalDate;
+
 public class Despesa {
-    
+    private String nomeDespesa;
+    private String descricaoDespesa;
+    private double valorDespesa;
+    private LocalDate data;
+
+    // Construtor
+    public Despesa(String nomeDespesa, String descricaoDespesa, double valorDespesa, LocalDate data) {
+        this.nomeDespesa = nomeDespesa;
+        this.descricaoDespesa = descricaoDespesa;
+        this.valorDespesa = valorDespesa;
+        this.data = data;
+    }
+
+    // métodos get e set
+    public String getNomeDespesa() {
+        return nomeDespesa;
+    }
+
+    public void setNomeDespesa(String nomeDespesa) {
+        this.nomeDespesa = nomeDespesa;
+    }
+
+    public String getDescricaoDespesa() {
+        return descricaoDespesa;
+    }
+
+    public void setDescricaoDespesa(String descricaoDespesa) {
+        this.descricaoDespesa = descricaoDespesa;
+    }
+
+    public double getValorDespesa() {
+        return valorDespesa;
+    }
+
+    public void setValorDespesa(double valorDespesa) {
+        this.valorDespesa = valorDespesa;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    // Q.3 - sobrescrever o método toString() de todas as classes implementadas
+    @Override
+    public String toString() {
+        return getNomeDespesa() + " " + getDescricaoDespesa() + " " + getValorDespesa() + " " + getData();
+    }
 }
